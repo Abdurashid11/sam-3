4ad16819207dd428208e30189e9305d598bca231
instagram.com <sanjar3370062@gmail.com>
2025-04-28 17:00:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleTheme() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    document.body.style.transition = "background-color 0.5s, color 0.5s";
+}
+
+function hel() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "blue";
+}
+
